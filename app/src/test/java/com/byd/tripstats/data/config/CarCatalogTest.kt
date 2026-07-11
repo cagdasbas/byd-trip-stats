533153a9c470f9de944eaefddd1fff01514bbed2
Angelos Oikonomou <angoikon@gmail.com>
2026-07-11 13:09:16 +0300

    Prepare for v2.12.0

--- app/src/test/java/com/byd/tripstats/data/config/CarCatalogTest.kt
+++ app/src/test/java/com/byd/tripstats/data/config/CarCatalogTest.kt
@@ -102,7 +102,7 @@ class CarCatalogTest {
         assertEquals(Drivetrain.FWD, CarCatalog.BYD_SEAL_U_DESIGN.drivetrain)
     }
 
-    @Test fun `catalog contains all 34 expected cars`() {
-        assertEquals(34, CarCatalog.allCars.size)
+    @Test fun `catalog contains all 36 expected cars`() {
+        assertEquals(36, CarCatalog.allCars.size)
     }
 }
